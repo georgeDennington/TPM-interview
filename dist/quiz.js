f15919d41031bbc2f2d5d4c423a73c91f6fc17af
george <georgedennington501@gmail.com>
2021-09-27 20:40:21 +0100

    quiz complete

--- dist/quiz.js
+++ dist/quiz.js
@@ -1,1 +1,32 @@
-console.log("its working");
+let question = 0;
+let answers = [];
+
+
+const submit = () => {
+  window.sessionStorage.setItem('answers', answers);
+  window.location.href = './answers.html';
+}
+
+const next = (e) => {
+  e.preventDefault();
+  var data = new FormData(form);
+  for (const [name,value] of data) {
+    answers.push(value);
+  }
+  question++;
+  form.reset();
+
+  if (question === 4) {
+    document.getElementById('submit').innerHTML = 'Submit';
+  }
+
+  if (question === 5) {
+    submit();
+  }
+}
+
+
+window.addEventListener('load', (event) => {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', next);
+});
